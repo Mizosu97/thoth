c02dadaeb8680a29512ac7a80c6925b729c05406
Mizosu97 <earthpig97@gmail.com>
2022-11-15 15:36:01 +0000

    f

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# gpt3-discord-chatbot
+# thoth
 An artificial intelligence chatbot for Discord that uses OpenAI's GPT-3 language model.
+
+*Thoth, (Greek), Egyptian Djhuty, in Egyptian religion, a god of the moon, of reckoning, of learning, and of writing*
+
+
+# How do I set it up?
+
+Thoth can be configured exactly how you want it. You can change everything from it's name to it's personality.
+
+## Prequisites
+- Create a discord bot application, and get it's token. Make one [here](https://discord.com/login?redirect_to=%2Fdevelopers%2Fapplications). Make sure the application has message content intent privileges.
+- Create an openAI account, then get an API key [here](https://beta.openai.com/account/api-keys).
+- Install the `git` and `python` packages provided by your distribution.
+- Install the `discord` and `openai` pip packages with `pip install discord openai`
+
+## Download the code
+thoth can be downloaded with the command `git clone https://github.com/Mizosu97/thoth`. Everything will be downloaded into a folder called `thoth`.
+
+Inside the `thoth` folder is a python file called `bot.py`. This is the main file you'll be working with.
+
+## Configuring the bot
+thoth needs to be configured to function correctly.
+
+1. Open the `bot.py` file in a text editor. Locate the configuration section.
+2. Set `BOT_NAME` to whatever you want the bot's name to be. I suggest making this the same as the discord bot's username. 
+**Example:** `BOT_NAME = "thoth"`
--- bot.py
+++ bot.py
@@ -2,9 +2,24 @@ import os
 import discord
 import openai
 
+
+# configuration
+
+BOT_NAME = ""
+
+BOT_DESCRIPTION = ""
+
 openai.api_key = ""
 
-TOKEN = ""
+DISCORD_TOKEN = ""
+
+DISCORD_BOT_ID = ""
+
+# configuration
+
+
+
+
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
@@ -12,13 +27,12 @@ client = discord.Client(intents=intents)
 
 completion = openai.Completion()
 
-scl = ""
 
 def ask(question, chat_log=None):
     if chat_log is None:
-        chat_log = scl
-    prompt = f'{chat_log}User: {question}\nmeiosu: '
-    res = completion.create(prompt=prompt, engine="davinci", stop=['User', '\n', 'meiosu'], temperature=0.9, top_p=1, frequency_penalty=0.8, presence_penalty=0.1, best_of=1, max_tokens=512)
+        chat_log = BOT_DESCRIPTION
+    prompt = f'{chat_log}User: {question}\n{BOT_NAME}: '
+    res = completion.create(prompt=prompt, engine="davinci", stop=['User', '\n', BOT_NAME], temperature=0.9, top_p=1, frequency_penalty=0.8, presence_penalty=0.1, best_of=1, max_tokens=512)
     ans = res.choices[0].text.strip()
     return ans
 
@@ -34,11 +48,11 @@ async def on_message(msg):
     if msg.author == client.user:
         return
 
-    if msg.content.startswith("<@!1041846368681074718> ") or msg.content.startswith("<@1041846368681074718>"):
+    if msg.content.startswith("<@!" + DISCORD_BOT_ID + "> ") or msg.content.startswith("<@" + DISCORD_BOT_ID + ">"):
         response = ask(msg.content.split(" ", 1)[1])
         print(response)
         await msg.channel.send(response)
 
 
-client.run(TOKEN)
+client.run(DISCORD_TOKEN)
 
